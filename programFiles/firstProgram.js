c36001011022a2b1d4152151127549532719e55e
MTC <mashjov13@outlook.com>
2022-12-23 18:19:32 -0600

    mi primer hello world en javascript

---
+++ programFiles/firstProgram.js
@@ -0,0 +1,10 @@
+/*  
+    firstProgram.js
+    AUTORA  : Maria Tchijov Cruz
+    FECHA   : 23 de diciembre de 2022.
+    Mi primer Hello World en Javascript usando console.log
+    NOTA: En Javascript no es necesario agregar semicolons al final de cada línea,
+        pero como estoy acostumbrada a Java, los incluiré en mis prácticas :)
+*/
+
+console.log("Hello world!");
