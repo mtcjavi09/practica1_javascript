2e851b329c9c56cbc0c8e4b7a6fc21267cf183fc
MTC <mashjov13@outlook.com>
2022-12-29 20:22:33 -0600

    se agrega file sobre uso de for para imprimir los números impares de 1 a 100

---
+++ loops/for.js
@@ -0,0 +1,13 @@
+/*  
+    for.js
+    AUTORA  : Maria Tchijov Cruz.
+    FECHA   : 29 de diciembre de 2022.
+    File donde estoy practicando el uso del ciclo for en JavaScript.
+*/
+
+//Se crea el ciclo for para imprimir los números impares de 1 a 100
+for (let i = 1; i <= 100; i++) 
+{
+    if((i % 2)!=0)
+    {console.log("Valor de i: ",i);}
+}
