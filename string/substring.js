a21d5f038af27616fc360557cbbb5514942a0fd1
MTC <mashjov13@outlook.com>
2023-01-04 18:43:35 -0600

    se agregan ejemplos de substr y substring en el file substring.js

--- string/substring.js
+++ string/substring.js
@@ -33,3 +33,11 @@ console.log("¿La palabra 'after' se encuentra al incio de str1?", str1.startsWi
 
 //ENDSWITH ayuda a verificar si un substring está presente al final de un string principal
 console.log("¿'always' es la palabra final de str1?", str1.endsWith(search1));
+
+//SUBSTR ayuda a cortar un string indicando el número de índice y el tamaño del recorte
+//  Si no agregas el tamaño, comenzará el recorte desde el índice indicado hasta el final del string
+console.log(str1.substr(0,search2.length));
+
+//SUBSTRING ayuda a cortar un string indicando el índice inicial y el índice final del recorte
+//  El segundo argumento también es opcional, por lo que si no se agrega pasará lo mismo que con substr
+console.log(str1.substring(str1.indexOf(search1))); //En este ejemplo no se agregó el segundo argumento
