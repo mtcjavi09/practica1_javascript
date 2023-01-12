973f382fe2d16c17889f457ddac6afdf75641740
MTC <mashjov13@outlook.com>
2023-01-11 20:07:33 -0600

    se agregan operaciones básicas para agregar y eliminar elementos al inicio y al final de un array

--- arrays/arrays.js
+++ arrays/arrays.js
@@ -20,5 +20,20 @@ console.log("Índice 6 en array 'integers':", integers[6]);
 console.log("Índice 6 en array 'WEEK':", WEEK[6], "\n");
 
 //Se muestra en consola el número de elementos de ambos arrays
-console.log("Hay", integers.length, "elementos en el array 'integers'.");
-console.log("Hay", WEEK.length, "elementos en el array 'WEEK'.\n");
+console.log("Hay", integers.length, "elementos en el array 'integers'");
+console.log("Hay", WEEK.length, "elementos en el array 'WEEK'\n");
+
+//Se agregan y eliminan elementos al inicio y al final del array integers, mostrando los cambios en consola
+integers.push(7,8,9,10,11); //Se agregan los elementos al final
+integers.unshift(-1,0); //Se agregan los elementos al final
+
+//Se muestran los cambios
+console.log("Array 'integers' actualizado:", integers, "\n");
+console.log("Ahora hay", integers.length, "elementos en el array 'integers'\n");
+
+integers.pop(); //Se elimina el último elemento, en este caso el número 11
+integers.shift(); //Se elimina el primer elemento, en este caso el número -1
+
+//Se muestran los cambios
+console.log("Array 'integers' final:", integers, "\n");
+console.log("Al finalizar la ejecución, hay", integers.length, "elementos en el array 'integers'\n");
